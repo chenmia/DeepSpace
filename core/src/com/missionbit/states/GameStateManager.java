5bfea81e380bbbb628ff983c4061fec6522321e6
timmydang27 <timmydang27@gmail.com>
2019-07-09 11:05:37 -0700

    State and GameStateManager done

--- core/src/com/missionbit/states/GameStateManager.java
+++ core/src/com/missionbit/states/GameStateManager.java
@@ -1,4 +1,33 @@
 package com.missionbit.states;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
 public class GameStateManager {
+
+    private Stack<State> states;
+
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    public void push(State state){
+        states.push(state);
+    }
+
+    public void pop(){
+        states.pop().dispose();
+    }
+
+    public void set(State state){
+        states.pop().dispose();
+        states.push(state);
+    }
+    public void update(float dt){
+        states.peek().update(dt);
+    }
+    public void render(SpriteBatch ab){
+        states.peek().render(ab);
+    }
 }
--- core/src/com/missionbit/states/State.java
+++ core/src/com/missionbit/states/State.java
@@ -1,4 +1,25 @@
 package com.missionbit.states;
 
-public class State {
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+
+public abstract class State {
+    protected PerspectiveCamera cam;
+    protected Vector3 mouse;
+    protected GameStateManager gsm;
+
+    protected State(GameStateManager gsm){
+        this.gsm = gsm;
+        cam = new PerspectiveCamera();
+        mouse = new Vector3();
+    }
+
+    public abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch ab);
+    public abstract void dispose();
+
+
 }
