5d25044f8f8b7c7c4e64c12c2883a3e295402a5a
chenmia <chen.ximia@gmail.com>
2019-07-09 10:00:04 -0700

    Initial commit

---
+++ desktop/src/com/missionbit/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.missionbit.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.missionbit.game.DeepSpace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DeepSpace(), config);
+	}
+}
